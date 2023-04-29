2a89dadc7439ba46870f34915055fd007309266f
brylimo <icj0103@gmail.com>
2023-04-30 02:01:43 +0900

    initialize server

---
+++ soogohaesseo/src/main/java/com/server/soogohaesseo/SoogohaesseoApplication.java
@@ -0,0 +1,13 @@
+package com.server.soogohaesseo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoogohaesseoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoogohaesseoApplication.class, args);
+	}
+
+}
---
+++ soogohaesseo/src/test/java/com/server/soogohaesseo/SoogohaesseoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.soogohaesseo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoogohaesseoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
