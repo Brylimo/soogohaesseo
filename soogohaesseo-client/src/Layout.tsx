43a7889d474cb1383ad593349c4fc4d0864171ab
brylimo <icj0103@gmail.com>
2023-05-01 00:47:35 +0900

    working on Footer component..

--- soogohaesseo-client/src/Layout.tsx
+++ soogohaesseo-client/src/Layout.tsx
@@ -1,12 +1,28 @@
 import { Outlet } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Header = styled.header`
+    display: flex;
+`;
+
+const Footer = styled.footer`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
 
 const Layout = () => {
     return (
         <div>
-            hiru
+            <Header>
+
+            </Header>
             <main>
                 <Outlet />
             </main>
+            <Footer>
+                DESIGN BY @Brylimo | Admin
+            </Footer>
         </div>
     );
 }
